f7208b1a21ae3c94fa169601e552831be3ccc520
QuanZhao <qz1217@163.com>
2019-08-17 13:00:25 +0800

    Add 'src/riscv/riscv_pkg.sv', 'src/tortoise_pkg.sv'.
    'riscv_pkg.sv' represents the information of the Specification, while
    'tortoise.sv' is more about the implementation details.

---
+++ src/riscv/riscv_pkg.sv
@@ -0,0 +1,195 @@
+/*
+ * File: riscv_pkg.sv
+ * Desc: common declarations of RISC-V, but no implementaion details
+ *
+ * Auth: QuanZhao
+ * Date: Aug-04-2019
+ *
+ * All the declarations come from the RISC-V specifications, some of them are
+ * defined according to the user configuration. Other informaitons like cache
+ * sizes, the issue algorithm and so on, should NOT be included here.
+ */
+
+`include "sysconfig.svh"
+
+package riscv_pkg;
+
+/* common structure */
+`ifdef  CONFIG_RV64I_SUPPORT
+    parameter   RV_XLEN = 64;      /* the widest supported XLEN */
+`else
+    parameter   RV_XLEN = 32;
+`endif
+
+typedef logic [RV_XLEN-1:0] data_t;
+typedef logic [RV_XLEN-1:0] addr_t;
+
+/* instruction formats */
+typedef enum logic [6:0] {
+    LOAD    = 7'B00_000_11,
+    LOADFP  = 7'b00_001_11,
+    CUSTOM0 = 7'b00_010_11,
+    MISCMEM = 7'b00_011_11,
+    OPIMM   = 7'b00_100_11,
+    AUIPC   = 7'b00_101_11,
+    OPIMM32 = 7'b00_110_11,
+
+    STORE   = 7'b01_000_11,
+    STOREFP = 7'b01_001_11,
+    CUSTOM1 = 7'b01_010_11,
+    AMO     = 7'b01_011_11,
+    OP      = 7'b01_100_11,
+    LUI     = 7'b01_101_11,
+    OP32    = 7'b01_110_11,
+
+    MADD    = 7'b10_000_11,
+    MSUB    = 7'b10_001_11,
+    NMSUB   = 7'b10_010_11,
+    NMADD   = 7'b10_011_11,
+    OPFP    = 7'b10_100_11,
+    RSRVD1  = 7'b10_101_11,
+    CUSTOM2 = 7'b10_110_11,
+
+    BRANCH  = 7'b11_000_11,
+    JALR    = 7'b11_001_11,
+    RSRVD2  = 7'b11_010_11,
+    JAL     = 7'b11_011_11,
+    SYSTEM  = 7'b11_100_11,
+    RSRVD3  = 7'b11_101_11,
+    CUSTOM3 = 7'b11_110_11
+} opcode_t;
+
+typedef struct packed {
+    logic [31:7]    rem;
+    opcode_t        opcode;
+} instr_t;
+
+typedef logic [4:0] reg_t;
+
+/* functions to get the immediate */
+/* verilator lint_off UNUSED */
+function automatic data_t i_imm(logic [31:7] rem);  /* 12 bits */
+    return {{(RV_XLEN-12){rem[31]}}, rem[31:20]};
+endfunction
+
+function automatic data_t s_imm(logic [31:7] rem);  /* 12 bits */
+    return {{(RV_XLEN-12){rem[31]}}, rem[31:25], rem[11:7]};
+endfunction
+
+function automatic data_t b_imm(logic [31:7] rem);  /* 12 bits, [12:1] */
+    return {{(RV_XLEN-13){rem[31]}}, rem[31], rem[7], rem[30:25], rem[11:8],
+        1'b0};
+endfunction
+
+function automatic data_t u_imm(logic [31:7] rem);  /* 20 bits, [31:12] */
+    return {{(RV_XLEN-32){rem[31]}}, rem[31:12], 12'b0};
+endfunction
+
+function automatic data_t j_imm(logic [31:7] rem);  /* 20 bits, [20:1] */
+    return {{(RV_XLEN-21){rem[31]}}, rem[31], rem[19:12], rem[20], rem[30:21],
+        1'b0};
+endfunction /* verilator lint_on UNUSED */
+
+/* exception */
+typedef enum logic [RV_XLEN-1:0] {
+    INSTR_ADDR_MISALIGNED   = RV_XLEN'(0),
+    INSTR_ACCESS_FAULT      = RV_XLEN'(1),
+    ILLEGAL_INSTR           = RV_XLEN'(2),
+    BREAKPOINT              = RV_XLEN'(3),
+    LD_ADDR_MISALIGNED      = RV_XLEN'(4),
+    LD_ACCESS_FAULT         = RV_XLEN'(5),
+    ST_ADDR_MISALIGNED      = RV_XLEN'(6),
+    ST_ACCESS_FAULT         = RV_XLEN'(7),
+    ENV_CALL_UMODE          = RV_XLEN'(8),    /* environment call from user mode */
+    ENV_CALL_SMODE          = RV_XLEN'(9),    /* environment call from supervisor mode */
+    ENV_CALL_MMODE          = RV_XLEN'(11),   /* environment call from machine mode */
+    INSTR_PAGE_FAULT        = RV_XLEN'(12),   /* Instruction page fault */
+    LOAD_PAGE_FAULT         = RV_XLEN'(13),   /* Load page fault */
+    STORE_PAGE_FAULT        = RV_XLEN'(15),   /* Store page fault */
+    DEBUG_REQUEST           = RV_XLEN'(24)    /* Custom: Debug request */
+} ex_cause_t;
+
+typedef logic [RV_XLEN-1:0] ex_tval_t;
+
+
+/* Privilege */
+// --------------------
+// Privilege Spec
+// --------------------
+typedef enum logic[1:0] {
+  PRIV_LVL_M = 2'b11,
+  PRIV_LVL_S = 2'b01,
+  PRIV_LVL_U = 2'b00
+} priv_lvl_t;
+
+// type which holds xlen
+typedef enum logic [1:0] {
+    XLEN_32  = 2'b01,
+    XLEN_64  = 2'b10,
+    XLEN_128 = 2'b11
+} xlen_t;
+
+typedef enum logic [1:0] {
+    OFF     = 2'b00,
+    INITIAL = 2'b01,
+    CLEAN   = 2'b10,
+    DIRTY   = 2'b11
+} xs_t;
+
+typedef struct packed {
+    logic         sd;     // signal dirty state - read-only
+    logic [62:36] wpri4;  // writes preserved reads ignored
+    xlen_t        sxl;    // variable supervisor mode xlen - hardwired to zero
+    xlen_t        uxl;    // variable user mode xlen - hardwired to zero
+    logic [8:0]   wpri3;  // writes preserved reads ignored
+    logic         tsr;    // trap sret
+    logic         tw;     // time wait
+    logic         tvm;    // trap virtual memory
+    logic         mxr;    // make executable readable
+    logic         sum;    // permit supervisor user memory access
+    logic         mprv;   // modify privilege - privilege level for ld/st
+    xs_t          xs;     // extension register - hardwired to zero
+    xs_t          fs;     // floating point extension register
+    priv_lvl_t    mpp;    // holds the previous privilege mode up to machine
+    logic [1:0]   wpri2;  // writes preserved reads ignored
+    logic         spp;    // holds the previous privilege mode up to supervisor
+    logic         mpie;   // machine interrupts enable bit active prior to trap
+    logic         wpri1;  // writes preserved reads ignored
+    logic         spie;   // supervisor interrupts enable bit active prior to trap
+    logic         upie;   // user interrupts enable bit active prior to trap - hardwired to zero
+    logic         mie;    // machine interrupts enable
+    logic         wpri0;  // writes preserved reads ignored
+    logic         sie;    // supervisor interrupts enable
+    logic         uie;    // user interrupts enable - hardwired to zero
+} status_rv64_t;
+
+typedef struct packed {
+    logic         sd;     // signal dirty - read-only - hardwired zero
+    logic [7:0]   wpri3;  // writes preserved reads ignored
+    logic         tsr;    // trap sret
+    logic         tw;     // time wait
+    logic         tvm;    // trap virtual memory
+    logic         mxr;    // make executable readable
+    logic         sum;    // permit supervisor user memory access
+    logic         mprv;   // modify privilege - privilege level for ld/st
+    logic [1:0]   xs;     // extension register - hardwired to zero
+    logic [1:0]   fs;     // extension register - hardwired to zero
+    priv_lvl_t    mpp;    // holds the previous privilege mode up to machine
+    logic [1:0]   wpri2;  // writes preserved reads ignored
+    logic         spp;    // holds the previous privilege mode up to supervisor
+    logic         mpie;   // machine interrupts enable bit active prior to trap
+    logic         wpri1;  // writes preserved reads ignored
+    logic         spie;   // supervisor interrupts enable bit active prior to trap
+    logic         upie;   // user interrupts enable bit active prior to trap - hardwired to zero
+    logic         mie;    // machine interrupts enable
+    logic         wpri0;  // writes preserved reads ignored
+    logic         sie;    // supervisor interrupts enable
+    logic         uie;    // user interrupts enable - hardwired to zero
+} status_rv32_t;
+
+typedef struct packed {
+    logic [3:0]  mode;
+    logic [15:0] asid;
+    logic [43:0] ppn;
+} satp_t;
+endpackage: riscv_pkg
---
+++ src/tortoise_pkg.sv
@@ -0,0 +1,135 @@
+/*
+ * File: tortoise_pkg.sv
+ * Desc: common declarations of the platform with implementaion details
+ *
+ * Auth: QuanZhao
+ * Date: Aug-04-2019
+ *
+ * We try to include all the platform-specific implementaion details here.
+ * Poeple who wants to adjust them should modify the user configuration file
+ * instead.
+ */
+
+`include "sysconfig.svh"
+
+package tortoise_pkg;
+
+import  riscv_pkg::*;
+
+parameter   INSTR_PER_FETCH = `CONFIG_INSTR_PER_FETCH;
+
+/* prediction */
+parameter   RAS_DEPTH   = `CONFIG_RAS_DEPTH;
+parameter   BHT_ENTRIES = `CONFIG_BHT_ENTRIES;
+parameter   BTB_ENTRIES = `CONFIG_BTB_ENTRIES;
+
+/* indicate how to update the branch history information */
+typedef enum logic [2:0] {
+    NO_BRANCH,      /* not a branch or jump instruction, no need to update */
+    DIRECT_JUMP,    /* jump to a known address, no need to update */
+    PREDICT_TAKEN,  /* predict whether the branch is taken, need to update */
+    PREDICT_TARGET  /* predict the target address, might need to update */
+} predict_t;
+
+typedef struct packed {
+    logic       valid;
+    ex_cause_t  cause;
+    ex_tval_t   tval;
+} exception_t;
+
+/* fetched instruction */
+typedef struct packed {
+    predict_t   instr_type;
+    logic       is_taken;       /* whether the branch is taken */
+    addr_t      target_addr;
+} sbe_predict_t;
+
+typedef struct packed {
+    logic       valid;
+    addr_t      addr;
+    instr_t     instr;
+    exception_t ex;
+    sbe_predict_t   predict;
+} fetch_entry_t;
+
+parameter   IFQ_DEPTH   = `CONFIG_IFQ_DEPTH;
+/* scoreboard */
+parameter   SB_ENTRIES  = `CONFIG_SB_ENTRIES;
+
+typedef logic [$clog2(SB_ENTRIES)-1:0] sb_idx_t;
+
+/* the functional unit to be used */
+typedef enum logic[3:0] {
+    FU_NONE,      // 0
+    FU_LOAD,      // 1
+    FU_STORE,     // 2
+    FU_ALU,       // 3
+    FU_BRANCH,     // 4
+    FU_MULT,      // 5
+    FU_CSR,       // 6
+    FU_FPU,       // 7
+    FU_FPU_VEC    // 8
+} fu_t;
+
+/* the operation to perform */
+typedef enum logic [6:0] {
+    // basic ALU op
+    ADD, SUB, ADDW, SUBW,
+    // logic operations
+    XORL, ORL, ANDL,
+    // shifts
+    SRA, SRL, SLL, SRLW, SLLW, SRAW,
+    // comparisons
+    BLTS, BLTU, BGES, BGEU, BEQ, BNE,
+    // jumps
+    JALR, BRANCH,
+    // set lower than operations
+    SLTS, SLTU,
+    // CSR functions
+    MRET, SRET, DRET, ECALL, WFI, FENCE, FENCE_I, SFENCE_VMA, CSR_WRITE, CSR_READ, CSR_SET, CSR_CLEAR,
+    // LSU functions
+    LD, SD, LW, LWU, SW, LH, LHU, SH, LB, SB, LBU,
+    // Atomic Memory Operations
+    AMO_LRW, AMO_LRD, AMO_SCW, AMO_SCD,
+    AMO_SWAPW, AMO_ADDW, AMO_ANDW, AMO_ORW, AMO_XORW, AMO_MAXW, AMO_MAXWU, AMO_MINW, AMO_MINWU,
+    AMO_SWAPD, AMO_ADDD, AMO_ANDD, AMO_ORD, AMO_XORD, AMO_MAXD, AMO_MAXDU, AMO_MIND, AMO_MINDU,
+    // Multiplications
+    MUL, MULH, MULHU, MULHSU, MULW,
+    // Divisions
+    DIV, DIVU, DIVW, DIVUW, REM, REMU, REMW, REMUW,
+    // Floating-Point Load and Store Instructions
+    FLD, FLW, FLH, FLB, FSD, FSW, FSH, FSB,
+    // Floating-Point Computational Instructions
+    FADD, FSUB, FMUL, FDIV, FMIN_MAX, FSQRT, FMADD, FMSUB, FNMSUB, FNMADD,
+    // Floating-Point Conversion and Move Instructions
+    FCVT_F2I, FCVT_I2F, FCVT_F2F, FSGNJ, FMV_F2X, FMV_X2F,
+    // Floating-Point Compare Instructions
+    FCMP,
+    // Floating-Point Classify Instruction
+    FCLASS,
+    // Vectorial Floating-Point Instructions that don't directly map onto the scalar ones
+    VFMIN, VFMAX, VFSGNJ, VFSGNJN, VFSGNJX, VFEQ, VFNE, VFLT, VFGE, VFLE, VFGT, VFCPKAB_S, VFCPKCD_S, VFCPKAB_D, VFCPKCD_D
+} fu_op;
+
+typedef struct packed {
+    logic   valid;  /* whether the value is valid */
+    reg_t   regno;  /* the value is stored in the register */
+    data_t  value;  /* the operand value */
+} operand_t;
+
+typedef struct packed {
+    logic       valid;
+    addr_t      pc;            // PC of instruction
+    sb_idx_t    trans_id;      // this can potentially be simplified, we could index the scoreboard entry
+                                             // with the transaction id in any case make the width more generic
+    fu_t        fu;            // functional unit to use
+    fu_op       op;            // operation to perform in each functional unit
+    operand_t   operand1;           // register source address 1
+    operand_t   operand2;           // register source address 2
+    operand_t   operand3;           // register source address 2
+    operand_t   result;
+    exception_t ex;            // exception has occurred
+    sbe_predict_t   predict;            // branch predict scoreboard data structure
+} scoreboard_entry_t;
+
+endpackage: tortoise_pkg
